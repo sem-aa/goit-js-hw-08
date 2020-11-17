6a583a18aa9b49926778da4c284bb09fbc03c0c7
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-11-17 10:19:47 +0200

    Форматирует Prettier

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 import images from './gallery-items.js';
 
-
 const jsGalleryContainer = document.querySelector('.js-gallery');
-const modalWindow = document.querySelector('.js-lightbox')
-const closeButtonModal = document.querySelector('[data-action="close-lightbox"]')
-const imageInModal = document.querySelector('.lightbox__image')
+const modalWindow = document.querySelector('.js-lightbox');
+const closeButtonModal = document.querySelector('[data-action="close-lightbox"]');
+const imageInModal = document.querySelector('.lightbox__image');
 
 const imagesGallery = creatJsGallery(images);
 
-jsGalleryContainer.insertAdjacentHTML('beforeend', imagesGallery)
+jsGalleryContainer.insertAdjacentHTML('beforeend', imagesGallery);
 
-jsGalleryContainer.addEventListener('click', onImageActive)
-closeButtonModal.addEventListener('click', offModalWindow)
+jsGalleryContainer.addEventListener('click', onImageActive);
+closeButtonModal.addEventListener('click', offModalWindow);
 
-
-function creatJsGallery (images){
-  return images.map(({preview, original, description}) => {
-    return `
+function creatJsGallery(images) {
+	return images
+		.map(({ preview, original, description }) => {
+			return `
     <li class="gallery__item">
   <a
     class="gallery__link"
@@ -31,20 +30,20 @@ function creatJsGallery (images){
   </a>
 </li>
 `;
-})
-.join('');
+		})
+		.join('');
 }
 
-function onImageActive (event) {
-  event.preventDefault();
- if (!event.target.classList.contains('gallery__image')) {
-  return;
- }
- modalWindow.classList.add('is-open')
- imageInModal.src = event.target.dataset.source
-};
+function onImageActive(event) {
+	event.preventDefault();
+	if (!event.target.classList.contains('gallery__image')) {
+		return;
+	}
+	modalWindow.classList.add('is-open');
+	imageInModal.src = event.target.dataset.source;
+}
 
-function offModalWindow (event){
-  modalWindow.classList.remove('is-open')
-  imageInModal.src = ''
+function offModalWindow(event) {
+	modalWindow.classList.remove('is-open');
+	imageInModal.src = '';
 }
